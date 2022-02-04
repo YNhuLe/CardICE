fb55eb55e08aee125ced39b9606c374be1730799
NhuYLe <lenhuy@sheridancollege.ca>
2022-02-04 09:37:07 -0500

    NhuY Le 991652865

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//NhuY Le
+//991652865
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
